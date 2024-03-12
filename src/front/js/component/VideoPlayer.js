3d43c29d934db1afe18e7c9e141dc33b18981409
Hugh Anderson <144385898+HughAnderson23@users.noreply.github.com>
2024-03-12 20:08:33 +0000

    not working

--- src/front/js/component/VideoPlayer.js
+++ src/front/js/component/VideoPlayer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import BurgerWalkAnimation from "src/front/img/BurgerWalkAnimation.gif";
+import BurgerWalkAnimation from "../../img/BurgerWalkAnimation.gif";
 
 const VideoPlayer = () => {
   return (
